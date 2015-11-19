b7c48c9b69def42c12d0120baccb54a2615088ac
Thorbjørn Hermansen <thhermansen@gmail.com>
2015-11-19 10:48:52 +0100

    Added StateMachine to common lib.
    Code gotten from Skalar, free to use.

---
+++ lib/echo_common/state_machine/machine.rb
@@ -0,0 +1,114 @@
+require 'echo_common/error'
+
+module EchoCommon
+  module StateMachine
+    #
+    # Contains functionality for a simple state machine
+    #
+    # You get two methods related to each event:
+    #   - `can_<event>?   Checks if you can transition via event
+    #   - `<event>!`      Transitions state via event, or fails with StateTransitionError
+    #
+    # Example of usage, after included in a class:
+    #
+    # event :run, from: ["standing", "walking"], to: "running"
+    # event :fly, from: "running", to: "flying", guard: -> { superman? }
+    #
+    # object = SomeStateMachine.new
+    # object.can_run?
+    # object.run!
+    #
+    module Machine
+        class StateTransitionError < EchoCommon::Error; end
+
+        def self.included(base)
+          base.class_eval do
+            def self.known_states
+              @@known_states ||= []
+            end
+            def self.known_states=(states)
+              @@known_states ||= states
+            end
+
+            extend ClassMethods
+          end
+        end
+
+        module ClassMethods
+          def event(name, from:, to:, guard: nil)
+            transition_to_state   = to.to_s
+            transition_from_states = Array(from).map(&:to_s)
+
+            self.known_states |= transition_from_states.dup.push(transition_to_state)
+
+            define_event_can_method_for name, from: transition_from_states, to: transition_to_state, guard: guard
+            define_event_method_for     name, to: transition_to_state
+            define_state_predicate_for  transition_to_state
+          end
+
+          private
+
+
+          def define_event_can_method_for(event_name, from:, to:, guard:)
+            define_method("can_#{event_name}?") do |raise_error: false|
+              # Check from -> to state
+              err = unless from.include?(state)
+                "Cannot transition from state #{state} to #{to}. Valid states for this transition is #{from.join(", ")}."
+              end
+
+              # Check guard
+              if err.nil?
+                passed_guard = case guard
+                when Symbol, String
+                  public_send(guard)
+                when Proc
+                  if guard.arity.zero?
+                    instance_exec &guard
+                  else
+                    guard.call self
+                  end
+                when nil
+                  true
+                else
+                  fail ArgumentError,
+                    "Guard may be method name (symbol or string), or a lambda / proc. Was: #{guard.inspect}"
+                end
+
+                unless passed_guard
+                  err = "Cannot transition from state #{state} to #{to}. Prohibited by guard."
+                end
+              end
+
+
+              if err.nil?
+                true
+              else
+                if raise_error
+                  fail StateTransitionError, err
+                else
+                  false
+                end
+              end
+            end
+          end
+
+          def define_event_method_for(event_name, to:)
+            define_method("#{event_name}!") do
+              if public_send("can_#{event_name}?", raise_error: true)
+                self.state = to
+              end
+            end
+          end
+
+          def define_state_predicate_for(state_name)
+            define_method("#{state_name}?") do
+              state == state_name
+            end
+          end
+        end
+
+
+
+    end
+  end
+end
---
+++ spec/lib/state_machine/machine_spec.rb
@@ -0,0 +1,123 @@
+require "spec_helper"
+require "echo_common/state_machine/machine"
+
+describe EchoCommon::StateMachine::Machine do
+  class Subject
+    include EchoCommon::StateMachine::Machine
+
+    attr_accessor :state, :have_chair, :superman
+
+    def initialize(state = "sitting")
+      @state = state
+      @have_chair = true
+      @superman = false
+    end
+
+    def save!
+    end
+
+    def self.scope(*args)
+    end
+
+    def have_chair?
+      !!@have_chair
+    end
+
+    def superman?
+      !!@superman
+    end
+
+    event :run, from: ["standing", "walking"], to: "running"
+    event :fly, from: "running", to: "flying", guard: -> { superman? }
+    event :land, from: "flying", to: "standing", guard: ->(object) { object.superman? }
+    event :walk, from: ["running", "standing"], to: "walking"
+    event :stop, from: ["running", "walking"], to: "standing"
+    event :sit, from: "standing", to: "sitting", guard: :have_chair?
+    event :stand, from: "sitting", to: "standing"
+  end
+
+  subject { Subject.new }
+
+  context "When sitting" do
+    it "can stand" do
+      subject.stand!
+      expect(subject.standing?).to be(true)
+    end
+
+    it "can't start running" do
+      expect { subject.run! }.to raise_error(EchoCommon::StateMachine::Machine::StateTransitionError)
+    end
+
+    it "can't start walking" do
+      expect { subject.walk! }.to raise_error(EchoCommon::StateMachine::Machine::StateTransitionError)
+    end
+  end
+
+  context "When standing" do
+    before { subject.stand! }
+
+    it "can start walking" do
+      subject.walk!
+      expect(subject.walking?).to be(true)
+    end
+
+    it "returns true for can_walk?" do
+      expect(subject.can_walk?).to be true
+    end
+
+    it "returns false for can_walk? when we are walking" do
+      subject.walk!
+      expect(subject.can_walk?).to be false
+    end
+
+    it "can start running" do
+      subject.run!
+      expect(subject.running?).to be(true)
+    end
+
+    it "can sit" do
+      subject.sit!
+      expect(subject).to be_sitting
+    end
+
+    it "can't sit if we don't have a chair" do
+      subject.have_chair = false
+      expect { subject.sit! }.to raise_error(EchoCommon::StateMachine::Machine::StateTransitionError)
+    end
+  end
+
+  context "When running" do
+    before do
+      subject.stand!
+      subject.run!
+    end
+
+    it "can start walking" do
+      subject.walk!
+      expect(subject.walking?).to be(true)
+    end
+
+    it "can stop" do
+      subject.stop!
+      expect(subject.standing?).to be(true)
+    end
+
+    it "can't sit down" do
+      expect { subject.sit! }.to raise_error(EchoCommon::StateMachine::Machine::StateTransitionError)
+    end
+
+    it "can't fly" do
+      expect { subject.fly! }.to raise_error(EchoCommon::StateMachine::Machine::StateTransitionError)
+    end
+
+    it "can fly and land when superman" do
+      subject.superman = true
+
+      subject.fly!
+      expect(subject).to be_flying
+
+      subject.land!
+      expect(subject).to be_standing
+    end
+  end
+end
