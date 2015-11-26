9eb9bd29b97d6445e8a6fdbea844dbbd1fe26fe3
Thorbjørn Hermansen <thhermansen@gmail.com>
2015-11-26 10:33:05 +0100

    Added Roar module to make all keys be dasherized.

---
+++ lib/echo_common/roar/dasherize_properties.rb
@@ -0,0 +1,30 @@
+require "lotus/utils/string"
+require "lotus/utils/hash"
+
+module EchoCommon
+  module Roar
+    # Module to be prepended to a Roar Representers to ensure all keys
+    # are dasherized.
+    module DasherizeProperties
+      def to_hash
+        HashWithDasherizeSupport.new(super).dasherize!
+      end
+
+
+      private
+
+      class HashWithDasherizeSupport < ::Lotus::Utils::Hash
+        def dasherize!
+          keys.each do |k|
+            v = delete(k)
+            v = HashWithDasherizeSupport.new(v).dasherize! if v.is_a?(::Hash)
+
+            self[::Lotus::Utils::String.new(k).dasherize.to_s] = v
+          end
+
+          self
+        end
+      end
+    end
+  end
+end
