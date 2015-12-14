97ed64c54a9b1ba03c4a8bdd093b5afda6027107
Thorbjørn Hermansen <thhermansen@gmail.com>
2015-12-14 21:15:56 +0100

    ValueObjectFromHash: Added the option to have default values.

--- lib/echo_common/utils/value_object_attributes_from_hash.rb
+++ lib/echo_common/utils/value_object_attributes_from_hash.rb
@@ -1,6 +1,8 @@
 module EchoCommon
   module Utils
     module ValueObjectAttributesFromHash
+      NO_VALUE = :no_value_given_as_fallback
+
       def self.included(base)
         base.class_eval do
           extend ClassMethods
@@ -8,9 +10,15 @@ module EchoCommon
       end
 
       module ClassMethods
-        def property(name, &block)
+        def property(name, default: NO_VALUE, &block)
           define_method name do
-            value = @attributes.fetch name.to_s
+            value = @attributes.fetch name.to_s do
+              if default != NO_VALUE
+                default
+              else
+                fail KeyError, "key not found: #{name.to_s}"
+              end
+            end
             value = block.call value if block
             value
           end
--- spec/lib/utils/value_object_attributes_from_hash_spec.rb
+++ spec/lib/utils/value_object_attributes_from_hash_spec.rb
@@ -8,6 +8,8 @@ module EchoCommon::Utils
 
       property(:number)
       property(:created_at) { |v| Time.parse v }
+      property(:fallback, default: {})
+      property(:no_value)
     end
 
     subject do
@@ -20,6 +22,14 @@ module EchoCommon::Utils
     it "has expected read methods" do
       expect(subject.number).to eq "123"
       expect(subject.created_at).to eq Time.parse "2015-06-01 02:00:00 +0200"
+    end
+
+    it "has a default value for fallback" do
+      expect(subject.fallback).to eq({})
+    end
+
+    it "raises error if property has no value and no default" do
+      expect { subject.no_value }.to raise_error KeyError
     end
 
     it "is equal to another object, with same values" do
