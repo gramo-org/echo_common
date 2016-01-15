4dd8f3cf91dd718b65713ce6eb891c7ade4c275e
kenglxn <ken@glxn.net>
2016-01-15 17:45:10 +0100

    Capitalize TZ names in spec

--- spec/lib/time_zone_spec.rb
+++ spec/lib/time_zone_spec.rb
@@ -4,8 +4,8 @@ module EchoCommon
   describe TimeZone do
     describe ".in_time_zone" do
       it "date.to_time creates time in correct time zone" do
-        minsk = TimeZone.in_time_zone("europe/minsk") { Date.new(2015, 1, 15).to_time }
-        oslo = TimeZone.in_time_zone("europe/oslo") { Date.new(2015, 1, 15).to_time }
+        minsk = TimeZone.in_time_zone("Europe/Minsk") { Date.new(2015, 1, 15).to_time }
+        oslo = TimeZone.in_time_zone("Europe/Oslo") { Date.new(2015, 1, 15).to_time }
 
         p "--------------"
         p minsk
@@ -19,8 +19,8 @@ module EchoCommon
       end
 
       it "Time.parse creates time in correct time zone" do
-        minsk = TimeZone.in_time_zone("europe/minsk") { Time.parse("2015-01-15 00:00:00") }
-        oslo = TimeZone.in_time_zone("europe/oslo") { Time.parse("2015-01-15 00:00:00") }
+        minsk = TimeZone.in_time_zone("Europe/Minsk") { Time.parse("2015-01-15 00:00:00") }
+        oslo = TimeZone.in_time_zone("Europe/Oslo") { Time.parse("2015-01-15 00:00:00") }
 
         expect(minsk.utc + (60*60*2)).to eq oslo.utc
       end
