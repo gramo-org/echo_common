2e6d426fd68beb14f58e8bad580d31709323e69d
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-08-11 09:45:57 +0200

    Make EchoCommon::RecordingDraft be compatible with main_artist as string.

--- lib/echo_common/recording_draft.rb
+++ lib/echo_common/recording_draft.rb
@@ -4,7 +4,7 @@ module EchoCommon
     attributes :title, :isrc, :duration_in_seconds
     attributes :recording_date, :release_date, :performer_note, :label_name
 
-    attributes :local_ids, :main_artists
+    attributes :local_ids, :main_artists, :main_artist
     attributes :composers
     attributes :alternative_isrcs, :alternative_titles
     attributes :tracks
@@ -17,6 +17,25 @@ module EchoCommon
       self.alternative_titles ||= []
       self.tracks ||= []
       self.composers ||= []
+    end
+
+    def main_artist
+      check_main_artist!
+      @main_artist || main_artists.join(' | ')
+    end
+
+    def main_artists
+      check_main_artist!
+      @main_artists
+    end
+
+
+    private
+
+    def check_main_artist!
+      if @main_artists && @main_artists.any? && !@main_artist.nil?
+        fail ArgumentError, "Cannot both have main_artists and main_artist set"
+      end
     end
   end
 end
