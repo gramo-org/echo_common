390db6523034ff32926418394f2020f6b9f2c55f
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-08-13 19:36:45 +0200

    Removed unnecessary included, base.class_eval in elastic query

--- lib/echo_common/services/elasticsearch/operations/query.rb
+++ lib/echo_common/services/elasticsearch/operations/query.rb
@@ -3,29 +3,25 @@ module EchoCommon
     class Elasticsearch
       module Operations
         module Query
-          def self.included(base)
-            base.class_eval do
-              # Wraps elasticsearch client 'search' method and returns the hits property
-              # of the result.
-              #
-              # Example client response:
-              #  {
-              #    "took"=>68,
-              #    "timed_out"=>false,
-              #    "_shards"=>{"total"=>5, "successful"=>5, "failed"=>0},
-              #    "hits"=>{
-              #      "total"=>0,
-              #      "max_score"=>nil,
-              #      "hits"=>[]
-              #    }
-              #  }
-              def search(query_body)
-                @client.search(
-                  index: @query_index,
-                  body: query_body
-                )
-              end
-            end
+          # Wraps elasticsearch client 'search' method and returns the hits property
+          # of the result.
+          #
+          # Example client response:
+          #  {
+          #    "took"=>68,
+          #    "timed_out"=>false,
+          #    "_shards"=>{"total"=>5, "successful"=>5, "failed"=>0},
+          #    "hits"=>{
+          #      "total"=>0,
+          #      "max_score"=>nil,
+          #      "hits"=>[]
+          #    }
+          #  }
+          def search(query_body)
+            @client.search(
+              index: @query_index,
+              body: query_body
+            )
           end
         end
       end
