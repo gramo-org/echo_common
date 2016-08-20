495e99dc8691b09d29dad012f4020def5668d32b
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-08-20 16:02:37 +0200

    Hanami Logger has changed a bit. Update our spec to match it's output.

--- lib/echo_common/logger/formatter.rb
+++ lib/echo_common/logger/formatter.rb
@@ -2,16 +2,24 @@ require 'hanami/logger'
 
 module EchoCommon
   module Logger
-    class Formatter < ::Hanami::Logger::Formatter
+    # Module to extend log formatters to include request_id if set on thread
+    module FormatterWithRequestId
       def call(severity, time, progname, msg)
         request_id = Thread.current[:echo_request_id]
 
         if request_id
-          progname = (progname.to_s + " [request_id=#{request_id}] ").lstrip
+          msg = {
+            request_id: request_id,
+            message: msg
+          }
         end
 
         super severity, time, progname, msg
       end
+    end
+
+    class Formatter < ::Hanami::Logger::Formatter
+      include FormatterWithRequestId
     end
   end
 end
