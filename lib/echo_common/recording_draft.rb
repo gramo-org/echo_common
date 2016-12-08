2b4a4e54bb8c33c31eb0d5d5036508f1b773dbf8
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-12-08 21:17:45 +0100

    Updated RecordingDraft with new schema.

--- lib/echo_common/recording_draft.rb
+++ lib/echo_common/recording_draft.rb
@@ -1,21 +1,66 @@
 # coding: utf-8
+
+require 'echo_common/entity'
+
 module EchoCommon
   class RecordingDraft < EchoCommon::Entity
-    attributes :title, :isrc, :duration_in_seconds
-    attributes :recording_date, :release_date, :performer_note, :label_name
+    class AlternativeIsrc < EchoCommon::Entity
+      attributes do
+        attribute :isrc,        Types::String
+        attribute :description, Types::String
+      end
+    end
 
-    attributes :local_ids, :main_artist
-    attributes :composers
-    attributes :alternative_isrcs, :alternative_titles
-    attributes :tracks
+    class AlternativeTitle < EchoCommon::Entity
+      attributes do
+        attribute :title,       Types::Strict::String
+        attribute :description, Types::Strict::String
+      end
+    end
 
-    def initialize(attributes = {})
-      super
-      self.local_ids ||= []
-      self.alternative_isrcs ||= []
-      self.alternative_titles ||= []
-      self.tracks ||= []
-      self.composers ||= []
+    class AlternativeName < EchoCommon::Entity
+      attributes do
+        attribute :name,        Types::Strict::String
+        attribute :description, Types::Strict::String
+      end
+    end
+
+    class Composer < EchoCommon::Entity
+      attributes do
+        attribute :name,              Types::Strict::String
+        attribute :alternative_names, Types::Coercible::Array.member(AlternativeName).default([])
+      end
+    end
+
+    class Track < EchoCommon::Entity
+      attributes do
+        attribute :title,           Types::Strict::String
+        attribute :side,            Types::Strict::Int
+        attribute :number,          Types::Strict::Int
+        attribute :label_name,      Types::Strict::String
+        attribute :isrc,            Types::Strict::String
+        attribute :release_id,      Types::Strict::String
+        attribute :release_title,   Types::Strict::String
+        attribute :catalog_number,  Types::Strict::String
+        attribute :p_line,          Types::Strict::String
+      end
+    end
+
+    attributes do
+      attribute :title,               Types::Strict::String
+      attribute :isrc,                Types::Strict::String
+      attribute :duration_in_seconds, Types::Strict::Int
+      attribute :recording_date,      Types::Strict::Date
+      attribute :release_date,        Types::Strict::Date
+      attribute :performer_note,      Types::Strict::String
+      attribute :label_name,          Types::Strict::String
+
+      attribute :main_artist,         Types::Strict::String
+      attribute :local_ids,           Types::Collection(Types::String).default([])
+      attribute :composers,           Types::Collection(Composer).default([])
+      attribute :alternative_isrcs,   Types::Collection(AlternativeIsrc).default([])
+      attribute :alternative_titles,  Types::Collection(AlternativeTitle).default([])
+      attribute :tracks,              Types::Collection(Track).default([])
     end
   end
 end
---
+++ spec/lib/recording_draft_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+require 'echo_common/recording_draft'
+
+module EchoCommon
+  describe RecordingDraft do
+    describe 'to_h' do
+      subject do
+        described_class.new(
+          title: 'Thriller',
+          isrc: 'ISRC123',
+          duration_in_seconds: 125,
+          recording_date: Date.new(2015, 10, 1),
+          composers: [{ name: 'Jackson' }],
+          alternative_titles: [{ title: 'Thriller!' }]
+        )
+      end
+
+      it 'returns the expected hash' do
+        hash = subject.to_h
+
+        expect(hash).to eq(
+          title: 'Thriller',
+          isrc: 'ISRC123',
+          duration_in_seconds: 125,
+          recording_date: Date.new(2015, 10, 1),
+          composers: [{ name: 'Jackson', alternative_names: [] }],
+          local_ids: [],
+          alternative_isrcs: [],
+          alternative_titles: [{ title: 'Thriller!' }],
+          tracks: []
+        )
+      end
+    end
+  end
+end
