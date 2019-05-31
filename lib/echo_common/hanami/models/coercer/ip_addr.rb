435f7fc5c10ffe1e97fab7d1c1a91a3838c52c33
Thorbjørn Hermansen <thhermansen@gmail.com>
2019-05-31 10:52:06 +0200

    fix: IPAddr::load works given an IPAddr object
    Previously it had to be a string or nil, but as sequel_pg now returns
    IPAddr objects when a row is read from the DB we must support this too.

--- lib/echo_common/hanami/models/coercer/ip_addr.rb
+++ lib/echo_common/hanami/models/coercer/ip_addr.rb
@@ -7,6 +7,7 @@ module EchoCommon
       module Coercer
         class IPAddr < ::Hanami::Model::Coercer
           def self.load(value)
+            return value if value.is_a? ::IPAddr
             ::IPAddr.new value unless value.nil?
           end
 
